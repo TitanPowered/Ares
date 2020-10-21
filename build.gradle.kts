bbd1faca8c83375474793c6d984bd9a3b303b5d3
PrimordialMoros <melodicdarkchaos@gmail.com>
2020-10-21 16:10:07 +0300

    Fetch Atlas from sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,12 @@ java {
 }
 
 repositories {
-    mavenLocal()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
     maven("https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly("me.moros", "Atlas", "1.0.0")
+    compileOnly("me.moros", "Atlas", "1.0.0-SNAPSHOT")
     compileOnly("com.destroystokyo.paper", "paper-api", "1.16.3-R0.1-SNAPSHOT")
 }
 
